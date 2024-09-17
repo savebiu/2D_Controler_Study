87cf16bf43762b27b1535ebada3fcf230faaba99
save_abc <134757660+savebiu@users.noreply.github.com>
2024-09-17 22:15:15 +0800

    添加墙壁检测和下滑速度

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -8,24 +8,28 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
-
+    
+    //动画状态
     private bool facingRight = true;
     private bool isWalking = true;
     private bool canJump = false;
     private bool isGround;
     private bool isJump;
     private int jumpCount = 2;
+    private bool isTouchingWall;
+    private bool isWallSliding;
 
     private float moveX;
     private float moveY;
 
-
-
     [Header("基本参数")]
     public float movementSpeed = 7.0f;
     public float jumpForce = 12f;
-    public Transform GroundCheck;
+    public float WallSlidingSpeed;
     public float groundCheckRadius;
+    public Transform GroundCheck;
+    public float WallCheckDistance;
+    public Transform WallCheck;
     public LayerMask whatIsGround;
     
 
@@ -44,18 +48,18 @@ public class PlayerController : MonoBehaviour
         CheckInput();
         UpdateAnimation();
         CheckIfJump();
-        
+        CheckIfWallSliding();
     }
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveX * movementSpeed, rb.velocity.y);
+        ApplyMovement();
         CheckSurroundings();
     }
+    //输入检测
     private void CheckInput()
     {
         moveX = Input.GetAxisRaw("Horizontal");
-        moveY = Input.GetAxisRaw("Vertical");
-        
+        moveY = Input.GetAxisRaw("Vertical");        
     }
  
     //移动
@@ -79,16 +83,29 @@ public class PlayerController : MonoBehaviour
         {
             Derection();
         }
+        //墙面下滑
+    }
+    private void ApplyMovement()
+    {
+        rb.velocity = new Vector2(moveX * movementSpeed, rb.velocity.y);
+        if(isWallSliding)
+        {
+            
+            if (rb.velocity.y < -WallSlidingSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -WallSlidingSpeed);                
+            }
+        }        
     }
     //翻转
     private void Derection()
-    {
-        
+    {        
         facingRight = !facingRight;
         Vector3 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
     }
+    //跳跃
     private void CheckIfJump()
     {
         //跳跃
@@ -114,11 +131,24 @@ public class PlayerController : MonoBehaviour
             jumpCount--;
         }   
     }
+    //地面反馈
     private void CheckSurroundings()
     {
         isGround = Physics2D.OverlapCircle(GroundCheck.position, groundCheckRadius, whatIsGround);
-        if(isGround)
+        if (isGround)
+        {
             jumpCount = 2;
+        }
+        isTouchingWall = Physics2D.Raycast(WallCheck.position, facingRight ? Vector2.right : Vector2.left, WallCheckDistance, whatIsGround);
+    }
+    //墙面反馈
+    private void CheckIfWallSliding()
+    {
+        if(isTouchingWall && !isGround && rb.velocity.y < 0)
+            isWallSliding = true;
+        else
+            isWallSliding = false;
+        Debug.Log(isWallSliding);
     }
     //动画
     private void UpdateAnimation()
@@ -129,7 +159,9 @@ public class PlayerController : MonoBehaviour
     }
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(GroundCheck.position, groundCheckRadius);       
+        Gizmos.DrawWireSphere(GroundCheck.position, groundCheckRadius);
+        Gizmos.DrawLine(WallCheck.position, new Vector3(WallCheck.position.x + WallCheckDistance, WallCheck.position.y, WallCheck.position.y));
     }
+
     
 }
